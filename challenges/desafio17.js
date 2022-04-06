8cb1540a3a338f0f455967138b5ed202a90b6964
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-04-05 23:43:08 -0300

    feat: desafios 17 a 21

---
+++ challenges/desafio17.js
@@ -0,0 +1,8 @@
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds",
+  totalProdutos: db.produtos.countDocuments({}),
+});
+db.resumoProdutos.findOne(
+  { franquia: "McDonalds" },
+  { _id: false, franquia: true, totalProdutos: true },
+);
